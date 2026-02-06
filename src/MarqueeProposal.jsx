ebfac20aba0d37adc9d3b2473eae89e068d9a20b
Ashish00746 <satuashish38@gmail.com>
2026-02-06 11:27:38 +0530

    Initial commit

---
+++ src/MarqueeProposal.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+
+const MarqueeProposal = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const sentences = [
+    "Poonoo, you light up my world like no one else.",
+    "Every moment with you feels like magic, my love.",
+    "My world begins and ends with you, Poonam.",
+    "Ashu's heart beats only for his Poonoo.",
+    "Life feels complete when you're around, Poonoo.",
+    "I can't imagine my future without you, my Jaan.",
+    "You're Ashu's sunshine on the darkest days.",
+    "Poonoo, you're the missing piece of Ashu's heart.",
+    "Will you be the love of my life forever, Poonam?",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+    }, 9000); // Change sentence every 9 seconds
+    return () => clearInterval(interval);
+  }, [sentences.length]);
+
+  return (
+    <div
+      style={{
+        width: "50%",
+        height: "75px",
+        margin: "50px auto",
+        borderRadius: "25px",
+        overflow: "hidden",
+        position: "relative",
+        background: "transparent",
+        boxShadow: "0 10px 30px rgba(0, 0, 0, 0.3)",
+        border: "4px solid #ffffff",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          whiteSpace: "nowrap",
+          position: "absolute",
+          animation: "marquee 10s linear infinite",
+        }}
+        key={currentIndex}
+      >
+        <span
+          style={{
+            fontSize: "2rem",
+            fontFamily: "Charm, serif",
+            // fontFamily: "Beau Rivage, serif", 
+            fontStyle: "normal",
+            fontWeight: "700",
+            color: "#191a19",
+            textShadow: "0 4px 8px rgba(0, 0, 0, 0.6)",
+          }}
+        >
+          {sentences[currentIndex]}
+        </span>
+      </div>
+      <style>{`
+        @keyframes marquee {
+          0% {
+            transform: translateX(100%); /* Start fully off-screen to the right */
+          }
+          100% {
+            transform: translateX(-100%); /* End fully off-screen to the left */
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default MarqueeProposal;
